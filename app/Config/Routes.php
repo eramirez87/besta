6afe584388c4364644cd0eb0afcb105424733b11
desarrolloCafira <desarrollo@carnesfinasramirez.com.mx>
2024-11-26 02:35:37 -0600

    Permite creacion de facturas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/api/getFacturas', 'FacturaController::getAll');
+$routes->post('/api/createFactura','FacturasController::create');
--- app/Controllers/FacturaController.php
+++ app/Controllers/FacturaController.php
@@ -7,6 +7,32 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class FacturaController extends BaseController
 {
+    public function create(){
+        $data = $this->request->getPost();
+        $db = \Config\Database::connect();
+        $query = $db->query(
+            'CALL updateOrCreateInvoice(?, ?, ?, ?, ?, ?, ?)', [
+                $data['cliente_id'],
+                $data['numero_factura'],
+                $data['fecha_emision'],
+                $data['monto_total'],
+                $data['estatus'],
+                $data['created_at'],
+                $data['updated_at']
+            ]
+        );
+        if ($query) {
+            return $this->response->setJSON([
+                'status' => 'success',
+                'message' => 'Factura procesada correctamente.'
+            ]);
+        } else {
+            return $this->response->setJSON([
+                'status' => 'error',
+                'message' => 'Hubo un error al procesar la factura.'
+            ], ResponseInterface::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
     public function getAll()
     {
         $db = \Config\Database::connect();
